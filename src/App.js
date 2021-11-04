df907114cfa31bdc21c1931c6de3b278b8a1803a
hristovs <hristovs@tcd.ie>
2021-11-04 20:19:59 +0000

    changes

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Source code:
+          Source code
         </a>
       </header>
     </div>
