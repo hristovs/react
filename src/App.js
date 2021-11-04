e81920cb62e295eab146f15f9763592acb5268f6
hristovs <hristovs@tcd.ie>
2021-11-04 20:18:47 +0000

    change link

--- src/App.js
+++ src/App.js
@@ -11,11 +11,11 @@ function App() {
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/hristovs/react"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Source code:
         </a>
       </header>
     </div>
